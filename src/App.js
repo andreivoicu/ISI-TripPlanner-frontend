d6fb8d084f8e4ee87991978754dc94632973de41
Andrei Voicu <voicuandrei04@gmail.com>
2024-12-04 13:21:28 +0200

    styled login/signup page to remove footer notch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from '@react-login-page/page8';
 
-const App = () => <Login/>;
+const App = () => <Login style={{ height: "100vh" }} />;
 
 export default App;
